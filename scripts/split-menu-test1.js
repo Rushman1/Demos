266a2958a49193eb114bd44105260060b1d2b1aa
Tim Savage <tim.savage@imail.org>
2024-02-07 14:59:11 -0800

    Working on Split Menu

--- scripts/split-menu-test1.js
+++ scripts/split-menu-test1.js
@@ -1,3 +1,42 @@
+let i = 0;
+let header = document.querySelector('header');
+let main = document.querySelector('main');
+let footer = document.querySelector('footer');
+let aqua = document.querySelector('.aqua');
+let number = document.querySelector('.number');
+let percentBar = document.querySelector('.percentBar');
+
+let interval = setInterval(function () {
+  number.innerHTML = `${i}<span>%</span>`;
+  percentBar.style.width = `${i}%`;
+  i++;
+  if (i == 101) {
+    clearInterval(interval);
+    setTimeout(() => {
+      fadeOut();
+    });
+  }
+}, 50);
+
+function fadeOut() {
+  let opacity = 1;
+  let timer = setInterval(function () {
+    if (opacity <= 0.1) {
+      clearInterval(timer);
+      aqua.style.opacity = 0;
+      aqua.remove();
+      header.style.opacity = '1';
+      header.style.visibility = 'visible';
+      main.style.opacity = '1';
+      main.style.visibility = 'visible';
+      footer.style.opacity = '1';
+      footer.style.visibility = 'visible';
+    }
+    aqua.style.opacity = opacity;
+    opacity -= opacity * 0.1;
+  }, 50);
+}
+
 let lastScrollTop = 0;
 navbar = document.getElementById('navbar');
 window.addEventListener('scroll', function () {
