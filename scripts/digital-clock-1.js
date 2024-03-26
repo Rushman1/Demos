bcc090bcfee83e40e79f201cd7773efd747dcb29
Tim Savage <tim.savage@imail.org>
2024-03-26 12:37:05 -0700

    Added Digital Clock, Responsive Tab Menu, Infinity Animation Effects pages (css, scss, js, html)

---
+++ scripts/digital-clock-1.js
@@ -0,0 +1,56 @@
+function clock() {
+  let secDots = document.getElementById('secDots');
+  let minDots = document.getElementById('minDots');
+  let hrDots = document.getElementById('hrDots');
+
+  var date = new Date();
+  var hours = date.getHours() % 12; //convert to 12-hour format
+  var amPm = date.getHours() >= 12 ? 'PM' : 'AM';
+  hours = hours === 0 ? 12 : hours; //handle midnight (0 hours)
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  var secondsDots = '';
+  for (var i = 1; i < 61; i++) {
+    var rotation = i * 6; // rotate each line by 6 degrees
+    if (i === seconds) {
+      secondsDots += `<div class="dot active" style="transform: rotate(${rotation}deg)"></div>`;
+    } else {
+      secondsDots += `<div class="dot" style="transform: rotate(${rotation}deg)"></div>`;
+    }
+  }
+  var minutesDots = '';
+  for (var i = 1; i < 61; i++) {
+    var rotation = i * 6; // rotate each line by 6 degrees
+    if (i === minutes) {
+      minutesDots += `<div class="dot active" style="transform: rotate(${rotation}deg)"></div>`;
+    } else {
+      minutesDots += `<div class="dot" style="transform: rotate(${rotation}deg)"></div>`;
+    }
+  }
+  var hoursDots = '';
+  for (var i = 1; i < 13; i++) {
+    var rotation = i * 30; // rotate each line by 6 degrees
+    if (i === hours) {
+      hoursDots += `<div class="dot active" style="transform: rotate(${rotation}deg)"></div>`;
+    } else {
+      hoursDots += `<div class="dot" style="transform: rotate(${rotation}deg)"></div>`;
+    }
+  }
+
+  secDots.innerHTML = `${secondsDots}<strong>${amPm}</strong><h2>${zero(
+    seconds
+  )}<br/><span>Seconds</span></h2>`;
+  minDots.innerHTML = `${minutesDots}<h2>${zero(
+    minutes
+  )}<br/><span>Minutes</span></h2>`;
+  hrDots.innerHTML = `${hoursDots}<h2>${zero(
+    hours
+  )}<br/><span>Hours</span></h2>`;
+}
+
+function zero(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+setInterval(clock, 1000);
